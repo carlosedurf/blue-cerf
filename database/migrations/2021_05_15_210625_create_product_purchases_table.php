90b1e7e94b9f327eecd0f1de3653c0dc8c3826a3
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-05-15 18:18:12 -0300

    feat: Adicione as migrations principais do sistema

---
+++ database/migrations/2021_05_15_210625_create_product_purchases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductPurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_purchases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('purchase_id')->constrained();
+            $table->string('name', 100);
+            $table->integer('quantity');
+            $table->decimal('purchase_price', 13, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_purchases');
+    }
+}
